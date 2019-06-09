14329b0406953b3cc2563351862441dcac012ded
Raymond Su <Raymond.Zu@walmartlabs.com>
2019-06-08 17:22:58 -0700

    data binding for property, event and two-way binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+  allowNewServer = true;
+  serverCreationStatus = 'No server was created!';
+  serverName = '';
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = false;
+    }, 2000);
+  }
 
   ngOnInit() {
   }
 
+  onCreativeServer() {
+    this.serverCreationStatus = 'Server is created, the server name is: ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) {
+    console.log(event);
+    this.serverName = (event.target as HTMLInputElement).value;
+  }
 }
